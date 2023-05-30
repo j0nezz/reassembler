7f71c9b5183b330735a136513b02842db7dc843d
Jonas Brunner <jonas.brunner@outlook.com>
2023-05-30 07:31:52 +0200

    add spoofed vs inferred distance evaluation

--- evaluation/spoofed_scenarios.py
+++ evaluation/spoofed_scenarios.py
@@ -1,3 +1,4 @@
+import numpy as np
 from matplotlib import pyplot as plt
 from netaddr import IPNetwork
 
@@ -48,3 +49,56 @@ def evaluate_spoofed_vs_discarded_nodes():
         summaries.append(summary)
 
     plot_spoofed_vs_discarded_nodes(summaries, spoofed_pct)
+
+
+def plot_spoofed_vs_inferred_distance(summaries, spoofed_pct):
+    wrong_distances = [np.array([d['inferred_distance_diff'] for k, d in summary['intermediate_nodes']['key_nodes'].items()]) for summary in summaries]
+    nr_key_nodes = [summary['intermediate_nodes']['nr_intermediate_nodes'] for summary in summaries]
+
+    def count_non_zero(arr):
+        return len(arr[arr != 0])
+
+    def average_err(arr):
+        return np.sum(np.abs(arr))/len(arr)
+
+    nr_wrong_distances = list(map(count_non_zero, wrong_distances))
+    pct_wrong = [e/n for e, n in zip(nr_wrong_distances, nr_key_nodes)]
+    err = list(map(average_err, wrong_distances))
+
+    plt.rc('font', size=15)
+
+    fig, ax1 = plt.subplots()
+    ax1.plot(spoofed_pct, pct_wrong, color='tab:blue')
+    ax1.set_ylabel('Pct. incorrect nodes', color='tab:blue')
+    ax1.yaxis.set_tick_params(labelcolor='tab:blue')
+    ax1.set_ylim(0, 0.5)
+
+    ax2 = ax1.twinx()
+
+    ax2.plot(spoofed_pct, err, color='tab:orange')
+    ax2.set_ylabel('Mean error in hops', color='tab:orange')
+    ax2.yaxis.set_tick_params(labelcolor='tab:orange')
+    ax2.set_ylim(0, 0.5)
+
+    ax1.set_xlabel('% Spoofed')
+
+    plt.tight_layout()
+    plt.savefig('distance-estimation-spoofed-1000.png', dpi=300)
+    plt.show()
+
+
+def evaluate_spoofed_vs_inferred_distance():
+    summaries = []
+    spoofed_pct = []
+    scenario = (
+        Generator([IPNetwork(f"{10 + i}.0.0.0/8") for i in range(15)], max_levels=6, max_clients=5, spoofed_pct=0)
+        .set_random_attack_sources(100))
+    for i in range(21):
+        spoofed_pct.append(i * 0.05)
+        print(f"Evaluating Run {i + 1} with {0.05 * i} spoofed sources")
+        fp = scenario.set_spoofed_pct(i * 0.05).simulate_attack().fingerprints
+        summary = Reassembler(fingerprint_data=fp).reassemble().add_ground_truth_data(scenario.target,
+                                                                                      scenario.sources).summary
+        summaries.append(summary)
+
+    plot_spoofed_vs_inferred_distance(summaries, spoofed_pct)
