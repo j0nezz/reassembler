0c70f4ff70a2bc7971ab7b647b6af4b9af208172
Jonas Brunner <jonas.brunner@outlook.com>
2023-06-08 17:51:58 +0200

    add background evaluation

---
+++ evaluation/background.py
@@ -0,0 +1,23 @@
+from netaddr import IPNetwork
+
+from generator import Generator
+from reassembler import Reassembler
+
+
+def evaluate_background_traffic():
+    n2 = Generator([IPNetwork(f"{10 + i}.0.0.0/8") for i in range(15)], max_levels=6, max_clients=5, spoofed_pct=0.25)
+    summaries = []
+    bgs = []
+    for i in range(1):
+        bg = 5000
+        scenario = n2.set_random_attack_sources(100).add_background_traffic(bg).simulate_attack()
+        print(f"Run {i} with {bg} background")
+        bgs.append(bg)
+        summary = Reassembler(fingerprint_data=scenario.fingerprints).reassemble(draw_percentiles=True).add_ground_truth_data(scenario.target, scenario.sources).summary
+        summaries.append(summary)
+
+
+
+
+
+
